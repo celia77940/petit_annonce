9bf71783cb80517e6c58e0363ecf837d902c03d1
celia77940 <celiarameau@live.fr>
2021-01-27 11:47:21 +0100

    regarde dans mon controllers

--- Controllers/annoncescontroler.php
+++ Controllers/annoncescontroler.php
@@ -5,14 +5,19 @@ use App\Models\AnnoncesModel;
 
 class AnnonceController
 {
+    
     public function index()
     {
+        // J'appelle ma classe AnnoncesModel
         $annoncesModel = new AnnoncesModel;
+        // Et je vais chercher la fonction find All
         $annonces = $annoncesModel->findAll();
 
-        header('Location: ../templates/annonces');
-
-
+        // Et la il faut mettre un trucs non ? Pour que sa renvoie a ma card annonce
+        // si tu va dans mon templates j'ai une table annonces
+        // et dedans j'ai fait une foreach et j'ai déclarer juste une données getitre pour voir si sa marche mais rien.
+        // Je pense qu'il manque quelque choses a faire et on y est depuis 8h30
+        //  A l'aide please
     }
 }
 
